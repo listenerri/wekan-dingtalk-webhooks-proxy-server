4befc729eee5e78700a0750538b661ea1f5c723d
listenerri <listenerri@gmail.com>
2021-04-09 14:58:41 +0800

    feat: add admin account config

--- configs.py
+++ configs.py
@@ -12,6 +12,7 @@ __Account_Config_File_Path = "config/config-account.json"
 
 __Server_Config = None
 __Account_Config = None
+__Admin_Accounts = {}
 
 __logger = None
 
@@ -21,6 +22,7 @@ def setup_logger(logger):
 
 def reload_server_config():
     global __Server_Config
+    global __Admin_Accounts
     if not os.path.isfile(__Server_Config_File_Path):
         __logger.error("Config file: {} not exists".format(__Server_Config_File_Path))
         return False
@@ -44,6 +46,9 @@ def reload_account_config():
         except Exception as e:
             __logger.error("Config file: {} load failed".format(__Account_Config_File_Path))
             return False
+    for wekan_user, user_info in __Account_Config.items():
+        if "admin" in user_info and user_info["admin"]:
+            __Admin_Accounts[wekan_user] = user_info
     return True
 
 def server_config():
@@ -51,3 +56,6 @@ def server_config():
 
 def account_config():
     return __Account_Config
+
+def admin_accounts():
+    return __Admin_Accounts
